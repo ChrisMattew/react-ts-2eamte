6f47e3ed6cea784e16c4b3a5a62bac0a34547bab
ChrisMattew <cristian.matteu@gmail.com>
2022-04-02 10:49:51 +0200

    2 files added

---
+++ components/TeamMood/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AvatarMood from '../AvatarMood';
+import './style';
+
+const team = [{}];
+x;
+
+const TeamMood = () => {
+  return (
+    <div className="team-mood-container">
+      <AvatarMood />
+    </div>
+  );
+};
+
+export default TeamMood;
