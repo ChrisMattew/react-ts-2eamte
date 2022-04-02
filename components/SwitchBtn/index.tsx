8c26d2631c49e76358ad7e197a49412962fbe48a
ChrisMattew <cristian.matteu@gmail.com>
2022-04-02 10:14:29 +0200

    2 files modified

--- components/SwitchBtn/index.tsx
+++ components/SwitchBtn/index.tsx
@@ -2,10 +2,12 @@ import * as React from 'react';
 import './style';
 
 const SwitchBtn = () => {
-  return <label className="switch">
-  <input type="checkbox"/>
-  <span className="slider round"></span>
-</label>;
+  return (
+    <label className="switch">
+      <input type="checkbox" />
+      <span className="slider round"></span>
+    </label>
+  );
 };
 
 export default SwitchBtn;
